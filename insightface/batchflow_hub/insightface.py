ba9acc4413c62ad0cd4ea2d0dec6e60483c9c3a5
tushr <tusharkolhe@gofynd.com>
2022-03-24 20:23:52 +0530

    InsightFace:  Handle negative detections

--- insightface/batchflow_hub/insightface.py
+++ insightface/batchflow_hub/insightface.py
@@ -16,7 +16,7 @@ class InsightFace(ModelProcessor):
     def __init__(
         self,
         root: str = None,
-        name:str= "buffalo_l",
+        name: str = "buffalo_l",
         model_source: Dict[str, str] = None,
         target_size=(160, 160),
         *args,
@@ -25,16 +25,23 @@ class InsightFace(ModelProcessor):
         # inject default model_source if model_path and model_source is None
 
         self.root = root or BATCHFLOW_HOME
-        self.name =  name
+        self.name = name
 
         if model_source is not None:
-            self._download_models(model_source, root=os.path.join(self.root, "models", self.name))
+            self._download_models(
+                model_source, root=os.path.join(self.root, "models", self.name)
+            )
         super().__init__(
-            model_path=os.path.join(BATCHFLOW_HOME, "models", self.name), model_source=model_source, *args, **kwargs
+            model_path=os.path.join(BATCHFLOW_HOME, "models", self.name),
+            model_source=model_source,
+            *args,
+            **kwargs,
         )
         self.target_size = target_size
-        self.model = FaceAnalysis(name=self.name, root=self.root,
-            providers=["CUDAExecutionProvider", "CPUExecutionProvider"]
+        self.model = FaceAnalysis(
+            name=self.name,
+            root=self.root,
+            providers=["CUDAExecutionProvider", "CPUExecutionProvider"],
         )
 
     @staticmethod
@@ -60,35 +67,31 @@ class InsightFace(ModelProcessor):
         self.model = None
         self._logger.info(f"Model Closed successfully")
 
-    def predict(self, image: np.asarray) -> np.asarray:
-        # convert np array to tf Tensor
-        image_tensor: tf.Tensor = tf.convert_to_tensor(image)
-        if len(image_tensor.shape) == 3:
-            # add batch
-            image_tensor = tf.expand_dims(image_tensor, axis=0)
-
-        output: np.asarray = self.model(image_tensor).numpy()
-        return output
-
     def postprocess(self, output):
         return output
+
+    def predict(self, image: np.asarray):
+        # rgb to bgr
+        image = image[..., ::-1]
+        return self.model.get(image)
 
     @log_time
     def process(self, ctx: Dict[str, Any]):
         """
         ctx signature:
-            image (List[np.asarray]): Image
+            image (np.asarray): Image
         """
         image: np.asarray = ctx.get("image")
-        faces = self.model.get(image[..., ::-1])
+        faces = self.predict(image)
         face_crops = []
         encodings = []
         detections = []
         for face in faces:
-            x1, y1, x2, y2 = list(map(int, face["bbox"]))
-            face_crops.append(image[y1:y2, x1:x2])
-            encodings.append(face["embedding"])
-            detections.append({"face": [x1, y1, x2, y2], "keypoints": face["kps"]})
+            x1, y1, x2, y2 = list(map(lambda p: max(0,int(p)) , face["bbox"]))
+            if ((x2-x1)*(y2-y1)>0):
+                face_crops.append(image[y1:y2, x1:x2])
+                encodings.append(face["embedding"])
+                detections.append({"face": [x1, y1, x2, y2], "keypoints": face["kps"]})
 
         return {
             "encodings": encodings,
@@ -101,122 +104,33 @@ class InsightFace(ModelProcessor):
     def process_batch(self, ctx: Dict[str, Any]) -> Any:
         """
         ctx signature:
-            face_crops (List[List[np.asarray]]): List of List of faces in RGB format
-            filename (List[str]):  image filename
-            image_id (List[str], optional): unique image id
-            detections (List[List[Dict[str, List[Any]]]]): Listface_detection of the format
-                        {"face":[x1,y1,x2,y2], "landmarks": {"right_eye":[x,y], "left_eye":[x,y], "nose":[x,y]}}
+            image (List[np.asarray]): Image
         """
-        face_crops: np.asarray = ctx.get("face_crops")
-
-        preprocessed_images = []
-        indexes = []
-
-        # make preprocessed images flat
-        for i, crops in enumerate(face_crops):
-            indexes.extend([i] * len(crops))
-            preprocessed_images.extend(self.preprocess(crops))
-
-        # batch the preprocessed image
-        batches = []
-        # batch 1
-        batch = []
-        for preprocessed_image in preprocessed_images:
-            batch.append(preprocessed_image)
-            if len(batch) == self.batch_size:
-                batches.append(batch)
-                batch = []
-
-        # append last batch
-        if len(batch) > 0:
-            batches.append(batch)
-
-        # process batch
-        encodings_flat = []
-        for batch in batches:
-            # concat images in batch
-            input_image: np.asarray = np.stack(batch, axis=0)
-            output = [np.expand_dims(o, axis=0) for o in self.predict(input_image)]
-            encodings_flat.extend(output)
-
+        images = ctx.get("image")
+        face_crops = []
         encodings = []
-        # l = len(np.unique(indexes)) +1
-        l = len(face_crops)
-        # create empty lists
-        for i in range(l):
-            encodings.append([])
-        for idx, enc in zip(indexes, encodings_flat):
-            encodings[idx].append(enc)
-        return {"encodings": encodings, **ctx}
+        detections = []
 
-    def preprocess(
-        self,
-        face_crops: List[np.asarray],
-        normalization: str = "base",
-    ) -> List[np.asarray]:
-        """
-        Preprocess input face image
-        Resize the face
-        Normalize the face
+        for image in images:
+            e = []
+            fc = []
+            det = []
+            faces = self.predict(image)
+            for face in faces:
+                x1, y1, x2, y2 = list(map(lambda p: max(0,int(p)) , face["bbox"]))
+                if ((x2-x1)*(y2-y1)>0):
+                    face_crops.append(image[y1:y2, x1:x2])
+                    encodings.append(face["embedding"])
+                    detections.append({"face": [x1, y1, x2, y2], "keypoints": face["kps"]})
 
-        Args:
-            face_crops (List[np.asarray]): List of face crop
-            normalization (str, optional): [image noramlization options (base, arcface)]. Defaults to "base".
+            face_crops.append(fc)
+            encodings.append(e)
+            detections.append(det)
 
-        Returns:
-            List[np.asarray]: [preprocessed face crops]
-        """
-        preprocessed_face_crops = []
-        for face_crop in face_crops:
-            if face_crop.shape[0] > 0 and face_crop.shape[1] > 0:
-                factor_0 = self.target_size[0] / face_crop.shape[0]
-                factor_1 = self.target_size[1] / face_crop.shape[1]
-                factor = min(factor_0, factor_1)
+        return {
+            "encodings": encodings,
+            "face_crops": face_crops,
+            "detections": detections,
+            **ctx,
+        }
 
-                dsize = (
-                    int(face_crop.shape[1] * factor),
-                    int(face_crop.shape[0] * factor),
-                )
-                face_crop = cv2.resize(face_crop, dsize)
-
-                # Then pad the other side to the target size by adding black pixels
-                diff_0 = self.target_size[0] - face_crop.shape[0]
-                diff_1 = self.target_size[1] - face_crop.shape[1]
-                if self.grayscale == False:
-                    # Put the base face_crop in the middle of the padded face_crop
-                    face_crop = np.pad(
-                        face_crop,
-                        (
-                            (diff_0 // 2, diff_0 - diff_0 // 2),
-                            (diff_1 // 2, diff_1 - diff_1 // 2),
-                            (0, 0),
-                        ),
-                        "constant",
-                    )
-                else:
-                    face_crop = np.pad(
-                        face_crop,
-                        (
-                            (diff_0 // 2, diff_0 - diff_0 // 2),
-                            (diff_1 // 2, diff_1 - diff_1 // 2),
-                        ),
-                        "constant",
-                    )
-
-            # ------------------------------------------
-
-            # double check: if target face_crop is not still the same size with target.
-            if face_crop.shape[0:2] != self.target_size:
-                face_crop = cv2.resize(face_crop, self.target_size)
-
-            if normalization == "base":
-                # normalize [0,1]
-                face_crop = face_crop.astype("float")
-                face_crop /= 255
-            elif normalization == "arcface":
-                # take from deepface normalization
-                face_crop -= 127.5
-                face_crop /= 128
-            preprocessed_face_crops.append(face_crop)
-
-        return preprocessed_face_crops
